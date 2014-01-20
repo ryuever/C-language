168d669551455f41407638f4228af6bd6b1bb5db
ryuever <liuyouchao111@gmail.com>
2014-01-20 19:04:24 +0900

    offsetof function is in stddef.h

--- function.c
+++ function.c
@@ -5,32 +5,39 @@
 struct student{
   char grade; /* char is 1 byte long */
   int  age;   /* int is 4 bytes long */
-};
+}tt = {1,378};
 
 int main(){
 
   // funtion sizeof 
   int sizeof_int[10];
   char char_arr[3];
-  printf("The length of a character is %zu, and the length of a interger is %zu\n", sizeof(char), sizeof (int));
+  printf("The length of a character is %zu, and the length of a interger is %zu\n", 
+         sizeof(char), sizeof (int));
   printf("The length of unsigned int is %zu.\n", sizeof(unsigned int));
   printf("The length of size_t is %zu. \n", sizeof(size_t));
   printf("The length of ssize_t is %zu. \n", sizeof(ssize_t));
   printf("The length of array sizeof_int is %zu.\n", sizeof(sizeof_int));
   printf("The length of char array is %zu.\n", sizeof(char_arr));
   printf("The length of struct student is %zu\n", sizeof (struct student));
+
   // applied to the name of a static array.The result will be the bytes of the whole array
   char *char_pointer_arr1[] = {
     "Liu", "Li" , "Du"
   };
-  printf("The length of char * array %zu.\n",sizeof(char_pointer_arr1));   // 24. because the element is a pointer
+  printf("The length of char * array %zu.\n",
+         sizeof(char_pointer_arr1));      // 24. because the element is a pointer
+
   // be used to calculate the number of elements in array.
-  printf("Number of elements in char * array %zu.\n",sizeof(char_pointer_arr1)/sizeof(char_pointer_arr1[1]));   // 3
+  printf("Number of elements in char * array %zu.\n",
+         sizeof(char_pointer_arr1)/sizeof(char_pointer_arr1[1]));   // 3
   printf("\n");
 
   // sizeof struct or union  -- pay attention to structure padding
-  printf("The length of struct student is %zu.\n", sizeof(struct student));               // 8 
-  printf("The offsetof(struct student, age) is %zu.\n", offsetof(struct student, age));   // 4,padding is 3; 
+  printf("The length of struct student is %zu.\n", 
+         sizeof(struct student));         // 8 
+  printf("The offsetof(struct student, age) is %zu.\n", 
+         offsetof(struct student, age));  // 4,padding is 3; 
   
   struct MixedData
   {
@@ -39,9 +46,12 @@ int main(){
     int Data3;
     char Data4;
   };
-  printf("The length of struct student is %zu.\n", sizeof(struct MixedData));                // 12 
-  printf("The offsetof(struct student, age) is %zu.\n", offsetof(struct MixedData, Data2));  // 2, padding for Data1 is 1
-  printf("The offsetof(struct student, age) is %zu.\n", offsetof(struct MixedData, Data4));  // 8
+  printf("The length of struct student is %zu.\n", 
+         sizeof(struct MixedData));             // 12 
+  printf("The offsetof(struct student, age) is %zu.\n", 
+         offsetof(struct MixedData, Data2));    // 2, padding for Data1 is 1
+  printf("The offsetof(struct student, age) is %zu.\n", 
+         offsetof(struct MixedData, Data4));    // 8
   printf("\n");
   /******************* function in stdio.h **********************/
 
@@ -94,7 +104,7 @@ int main(){
   printf("memcpy1[4] is %c\n", memcpy1[4]);     // 'o'
   printf("\n");
 
-  /******************* function in string.h **********************/  
+  /******************* function in stddef.h **********************/  
   // offsetof(type, member)
   printf("offsetof(struct student,age) is %zu.\n", offsetof(struct student,age));
   printf("\n");
