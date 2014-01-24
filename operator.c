285e7359537e5e338a5cf4b465f78c16250e9a0b
ryuever <liuyouchao111@gmail.com>
2014-01-24 20:19:13 +0900

    compound assignment op=

--- operator.c
+++ operator.c
@@ -1,21 +1,54 @@
+//=============================================== file = operator.c =============
+//=  A tutorial program of C operator                                           =
+//===============================================================================
+//=  Notes:                                                                     =
+//=    1) To make it easier to refer to, I have listed typedef statements       =
+//=       in front of the block it is first referred to.                        =
+//=-----------------------------------------------------------------------------=
+//= Example :                                                                   =
+//=                                                                             =
+//=-----------------------------------------------------------------------------=
+//= Example output                                                              =
+//=-----------------------------------------------------------------------------=
+//=  Build: gcc -g operator.c                                                   =
+//=-----------------------------------------------------------------------------=
+//=  Author: Liu Youchao                                                        =
+//=-----------------------------------------------------------------------------=
+//=  History:                                                                   =
+//===============================================================================
+//----- Include files -----------------------------------------------------------
 #include <stdio.h>
 
 int main(){
-  
   int x = 1, y=2;
+//==============================================================================
+//                           binary operators  &,|
+// Bitwise complement : the tilde, ~, flips every bit. if you have a 1, it's 
+//                      a 0, and if you have a 0, it's a 1. 
+// compared with Bor, '~' just need one operand for this operator.
+//==============================================================================
+  int bcmt;
+  bcmt = ~x;
+  printf("      ~x(%d) = %d.\n", x,bcmt);
+
+  // Bitwise or, which need two operands.
+  int bor;
+  bor = x | y;
+  printf("x(%d) | y(%d) = %d.\n",x,y,bor);
   
-  /* binary operators  &,| */
-
-  // Bitwise complement : the tilde, ~, flips every bit. if you have a 1, it's a 0, and if you have a 0, it's a 1. 
-  // compared with Bor, we just need one operand for this operator.
-  int Bct;
-  Bct = ~x;
-  printf("The Binary complement of %d is %d.\n", x,Bct);
-
   // Bitwise and, which need two operands.
-  int Band;
-  Band = x & y;
-  printf("Bitwise and of %d and %d is %d.\n", x,y,Band);
+  int band;
+  band = x & y;
+  printf("x(%d) & y(%d) = %d.\n", x,y,band);
+
+//==============================================================================
+//                          compound assignment 'op='
+// where op can be any one of the ten operator symbols '*', '/', '%', '+', '-', 
+// '<<', '>>', '&', '^' or '|'. 
+//==============================================================================
+  int cmpd1 = 5;
+  cmpd1 |= 3;
+  printf("      cmpd1 = %d.\n",cmpd1);
 
   return 0;
 }
