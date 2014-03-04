7c70005505f7347d10002c71857e06a595c53da9
ryuever <liuyouchao111@gmail.com>
2014-03-04 18:52:01 +0900

    The EOF

--- file.c
+++ file.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(){
 //==============================================================================
@@ -10,12 +11,12 @@ int main(){
 // ssize_t read(int fd, void *buf, size_t count);
 //==============================================================================
   int filedesc = open("test.txt",O_RDWR);
-  printf("%d",filedesc);
-  if(filedesc < 0)
+  printf("file descriptor : %d\n",filedesc);
+  if(filedesc < 0)                  // if return value is negative, exit.
     return 1;
   
   if(write(filedesc, "This will be output to test.txt\nI am lily. nice to meet you!\n",61) != 61){
-    write(2,"There was an error writing to test.txt\n",43);
+    write(2,"There was an error writing to test.txt\n",39);   // file descriptor 2 is stand error
     return 1;
   }
 
@@ -41,6 +42,54 @@ int main(){
   int nread2 = read(filedesc2,buf2,40);
   printf("buf2 is : %s",buf2); 
   printf("nread2 is : %d\n",nread2);
+
+//==============================================================================
+//                    EOF : End of FIle 
+// EOF = -1. It doesn't exist in file. In other word, it don't occupy byte in 
+// file.
+//==============================================================================
+  int filedesc4 = open("test2.txt",O_RDWR | O_CREAT);
+  write(filedesc4, "",10);
+  char str[20];
   
-  return 0;
+  int nread4 = read(filedesc4, str, 10);
+  
+  printf("Reading a empty file, it will return 0.\n");
+  printf("nread4 is : %d\n",nread4);    // nread4 = 0.no char is read. 
+  
+  printf("EOF is %x.\n" ,EOF);  
+//===============================================================================
+// if file not existing. The pointer to file will be Null
+//===============================================================================
+  FILE * fp;
+  int c;           // must be declared to integer to receive fgetc(fp)
+  if((fp = fopen("test.txt","rb")) == NULL){
+    printf("file can't be opened.\n");
   }
+
+  if((c = fgetc(fp)) == EOF){
+    perror("reach the end of file.\n");
+  }
+  fclose(fp);
+//===============================================================================
+//                              EOF trap
+// $  gcc -g -fsigned-char traps.c      // stop at the third char oxff
+// $  gcc -g -funsigned-char traps.c    // go into loop
+//===============================================================================
+  FILE *fp2 = fopen("test.txt","r+");
+  fputs("\x35\x38\xff\x32\x33",fp);
+  fclose(fp2);
+
+  char c3;
+  FILE *fp3 = fopen("test.txt","r");
+  while ((c3 = fgetc(fp3)) != EOF){
+    printf ("read c2 = '%c'\n", c);
+  }
+
+  fclose(fp);
+
+//===============================================================================
+//                             The End
+//===============================================================================
+  return 0;
+}
